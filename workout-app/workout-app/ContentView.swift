970b505dced5e5ffc5e91ad2ec59c2ee88797cb6
Pierre CAILLET <90714496+Pierrooooo@users.noreply.github.com>
2023-11-20 08:00:21 -0800

    :tada:

---
+++ workout-app/workout-app/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  workout-app
+//
+//  Created by CAILLET Pierre on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Link("View Our Terms of Service",
+                  destination: URL(string: "ExercisesView")!)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ workout-app/workout-app/workout_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  workout_appApp.swift
+//  workout-app
+//
+//  Created by CAILLET Pierre on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct workout_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
